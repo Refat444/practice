6230301e9809f319cd2f3b3e6709a0f6300301e4
Refat <mdrefa444@gmail.com>
2022-01-30 22:01:25 +0600

    text file

---
+++ page/text.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p> My name is Refat </p>
+    <p> for add= git add -A
+        for commit= git commit -m "XYZ"
+        for push= git push 
+        for pull= git pull
+        for status= git status 
+</p>
+
+</body>
+</html>
